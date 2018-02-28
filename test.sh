d8d580a73074a31a45ca7dc7e11beac767b528f9
Martin Fox <martinfox@yasabe.com>
2018-02-27 20:03:48 -0800

    Fix pom and scripts

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-mvn clean install
-java co.softwarebox.test.StressTest
+git pull
 
+mvn test || exit 1
+
+mvn clean install || exit 1
+
+echo --------------------------------------------
+echo STARTING APP
+echo --------------------------------------------
+
+java -jar target/urls-stress-test-1.0.0.jar
 
